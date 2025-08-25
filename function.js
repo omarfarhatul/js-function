2e3d0f169b3ffa4c5816992bfcaae5dcc8be872f
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-25 11:11:15 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,20 @@
+const price = 45;
+//  function declare
+function fun1(){
+    console.log('Go to home.');
+    console.log('Run on the road.');
+}
+// call the function
+fun1();
+
+
+
+function fun2(){
+    console.log('Awesome!');
+    console.log('Hi!.');
+}
+fun2();
+
+console.log('...........')
+console.log('Wait a minute.');
+fun2();
