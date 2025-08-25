8d27ea3aa449164dd327abf79543a961b5e4cbf9
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-25 11:36:13 +0600

    Update

---
+++ return.js
@@ -0,0 +1,14 @@
+function tentimes(num){
+    const result=num*10;
+    return result;
+}
+const output1=tentimes(4);
+console.log(output1);
+
+// divided
+function half(n){
+    const result=n/2;
+    return result;
+}
+const output2=half(99);
+console.log(output2);
