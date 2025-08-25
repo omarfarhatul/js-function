959ae6165b0816c199c9921060285a20dcb27139
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-25 18:34:45 +0600

    Update

--- array-op.js
+++ array-op.js
@@ -1,7 +1,17 @@
 // objective: write a function to give me the sum of all numbers in an array
-function sumOfNumbers(){
-    
+// step-1: declare a function
+// step-2: call check whether the function is called properly
+// step-3: set a parameter(s)
+// step-4: pas the papameter(s), check whether papameter is passed in a proper format
+// step-5: do the function tasks (step by step)
+function sumOfNumbers(numbers){
+    let sum=0;
+    for(const number of numbers){
+        console.log(number);
+        sum=sum+number;
+    }
+    return sum;
 }
-
-const sum=sumOfNumbers();
+const num=[354, 243, 65, 232];
+const sum=sumOfNumbers(num);
 console.log('sum of number is: ', sum);
