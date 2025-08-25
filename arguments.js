fb45c93811aa79482c0c7441548e57d258a3eaa6
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-25 11:30:31 +0600

    Updated

--- arguments.js
+++ arguments.js
@@ -1,6 +1,6 @@
 function doubleIt(num){
     const doubled = num*2;
-    console.log(doubled);
+    console.log(num, doubled);
 }
 console.log('I will call the function')
 doubleIt(64);
@@ -8,3 +8,18 @@ console.log('---------')
 doubleIt(87);
 console.log('---------')
 doubleIt(23);
+
+const num = 33;
+doubleIt(num);
+const money=145;
+doubleIt(money);
+
+
+// difference
+function difference(n1, n2){
+    const diff = n1-n2;
+    console.log(n1, n2, 'difference is: ', diff);
+}
+const fatherAge=50;
+const myAge=25;
+difference(fatherAge, myAge);
