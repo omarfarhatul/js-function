6b4002c880c47119e356517a8fcfc693d3ad2111
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-25 11:25:08 +0600

    Add arguments js

---
+++ arguments.js
@@ -0,0 +1,10 @@
+function doubleIt(num){
+    const doubled = num*2;
+    console.log(doubled);
+}
+console.log('I will call the function')
+doubleIt(64);
+console.log('---------')
+doubleIt(87);
+console.log('---------')
+doubleIt(23);
