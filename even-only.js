bb1373c7cdc596b955695306e6b08e92ad664985
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-25 19:03:43 +0600

    Update

--- even-only.js
+++ even-only.js
@@ -8,6 +8,21 @@ function evenNumbersOnly(numbers){
         }
     }
 }
-
 const numbers =[3, 5, 23, 21, 6];
 evenNumbersOnly(numbers);
+
+// sum of even number
+function sumOfevenNumbers(nums){
+    let sum=0;
+    for(const num of nums){
+        if(num%2===0){
+            console.log(num);
+            sum=sum+num;
+        }
+    }
+    return sum;
+}
+
+const nums=[3, 6, 290, 8, 13];
+const sum= sumOfevenNumbers(nums);
+console.log('sum of the even number is: ', sum);
